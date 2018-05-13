53a4c2b6d5bfc8048bbc9e57a54ff82d002a76e5
Gia <gianina.oui@gmail.com>
2018-05-13 02:31:10 -0700

    ''

--- eRoutingSlip/Controllers/AccountController.cs
+++ eRoutingSlip/Controllers/AccountController.cs
@@ -173,7 +173,7 @@ namespace eRoutingSlip.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("SetProfile", "Employee", new { id = e.EmployeeID });
                 }
                 AddErrors(result);
             }
