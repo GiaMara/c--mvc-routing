eb8a8b07f972fb3bea2cf4281f7ec36446976765
Gia <gianina.oui@gmail.com>
2018-05-13 01:44:10 -0700

    Emp added in AccountController

--- eRoutingSlip/Controllers/AccountController.cs
+++ eRoutingSlip/Controllers/AccountController.cs
@@ -161,6 +161,12 @@ namespace eRoutingSlip.Controllers
                 {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
+                    ERoutingSlipDB _db = new ERoutingSlipDB();
+
+                    Employee e = new Employee();
+                    e.Email = model.Email;
+                    _db.Employees.Add(e);
+                    _db.SaveChanges();
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
